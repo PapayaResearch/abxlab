cec988d41cbd507a4bda46e65fa07f43915abeaf
Manuel Cherep <manuelcherep@gmail.com>
2025-04-19 21:18:05 -0400

    Better structuring agent_lab_run.py

--- agent_lab_run.py
+++ agent_lab_run.py
@@ -1,27 +1,42 @@
 import os
-from agentlab.agents.generic_agent import AGENT_4o_MINI, AGENT_3_5
-from agentlab.experiments.study import make_study
+import logging
+
+from agentlab.agents.generic_agent import (
+    AGENT_LLAMA3_70B,
+    AGENT_LLAMA31_70B,
+    RANDOM_SEARCH_AGENT,
+    AGENT_4o,
+    AGENT_4o_MINI,
+    AGENT_o3_MINI,
+    AGENT_o1_MINI,
+    AGENT_37_SONNET,
+    AGENT_CLAUDE_SONNET_35,
+)
+from agentlab.experiments.study import Study
+
+AGENT_ARGS = [AGENT_4o_MINI]
+BENCHMARK = "nudgingarena_tiny"
 
 def main():
+    logging.getLogger().setLevel(logging.INFO)
+
     os.environ["AGENTLAB_EXP_ROOT"] = os.path.abspath("agent_lab_exp")
 
     os.environ[
         "WA_SHOPPING"
     ] = "http://matlaber12.media.mit.edu:7770/"
 
-    # # if your webarena instance offers the FULL_RESET feature (optional)
-    # os.environ["WA_FULL_RESET"] = f"{BASE_URL}:7565"
-
-    # # otherwise, be sure to NOT set WA_FULL_RESET, or set it to an empty string
-    # os.environ["WA_FULL_RESET"] = ""
-
-    study = make_study(
-        benchmark="nudgingarena_tiny",  # or "webarena", "workarena_l1" ...
-        agent_args=[AGENT_4o_MINI],
-        comment="test with nudgingarena",
+    study = Study(
+        AGENT_ARGS,
+        BENCHMARK,
+        logging_level_stdout=logging.WARNING
     )
 
-    study.run(n_jobs=1)
+    study.run(
+        n_jobs=1,
+        parallel_backend="ray",
+        n_relaunch=1
+    )
 
 if __name__ == '__main__':
     # The code needs to be inside this to avoid issues with multiprocessing in macOS

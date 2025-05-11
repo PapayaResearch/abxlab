89bce76c6249c163927b276d5bba64f8a71fd6cd
Nikhil Singh <nikhilsinghmus@gmail.com>
2025-05-11 13:02:19 -0400

    Add continue param + simplify config.

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import os
+import glob
 import datetime
 import logging
+import json
 import dotenv
 dotenv.load_dotenv()
 os.environ["AGENTLAB_EXP_ROOT"] = os.path.join( # Make unique run directory (mainly for gathering multiruns)
@@ -23,6 +25,23 @@ def main(cfg: DictConfig):
     logging.getLogger("bs4.dammit").setLevel(logging.CRITICAL)
     log = logging.getLogger(__name__)
 
+    # Check if we want to continue a previous experiment set
+    study_dir = (Path(cfg.experiment.root_dir) / cfg.task.name).absolute()
+    if cfg.experiment.continue_from:
+        study_dir_from_prev = (Path(cfg.experiment.continue_from) / cfg.task.name).absolute()
+        if study_dir_from_prev.exists():
+            # Check if the experiment ran *correctly*
+            summary_info = glob.glob(os.path.join(study_dir_from_prev, "**", "*", "summary_info.json"), recursive=True)
+            assert len(summary_info) == 1, "There should be exactly one summary_info.json file"
+            with open(summary_info[0]) as json_file:
+                summary_info = json.load(json_file)
+
+            if (summary_info["err_msg"] is None) and (summary_info["stack_trace"] is None):
+                log.info("Skipping %s; it seems to have run correctly.", cfg.task.name)
+                return 0
+            else:
+                log.info("Re-running %s; it seems to have run incorrectly.", cfg.task.name)
+
     # Instantiate agent and benchmark directly from Hydra configs
     agent = hydra.utils.instantiate(cfg.agent)
     benchmark = hydra.utils.instantiate(cfg.benchmark, _partial_=True)(
@@ -33,7 +52,6 @@ def main(cfg: DictConfig):
     )
 
     # Register the env here, so we don't need to reach into BrowserGym
-    study_dir = (Path(cfg.experiment.root_dir) / cfg.task.name).absolute()
     gym.register(
         id=f"browsergym/nudgelab.{cfg.task.name}",
         entry_point=lambda *env_args, **env_kwargs: NudgeLabBrowserEnv(

d105e14458b0c12c350770aadf6e8e9b50fb1d7c
Manuel Cherep <manuelcherep@gmail.com>
2025-07-22 00:30:39 -0400

    Update download script and CSVs

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,16 +1,13 @@
 mkdir -p tasks
 
-# Download products.csv
-wget --header -O tasks/products.csv "https://docs.google.com/spreadsheets/d/15LA25I-z8uz6UaF6tXZ9MEMkgbFvanUYTJqlbKdPnhI/export?gid=1737865517&format=csv"
-
 # Download categories.csv
-wget --header -O tasks/categories.csv "https://docs.google.com/spreadsheets/d/15LA25I-z8uz6UaF6tXZ9MEMkgbFvanUYTJqlbKdPnhI/export?gid=1365867936&format=csv"
+wget -O tasks/categories.csv "https://docs.google.com/spreadsheets/d/15LA25I-z8uz6UaF6tXZ9MEMkgbFvanUYTJqlbKdPnhI/export?gid=1365867936&format=csv"
 
 # Download home.csv
-wget --header -O tasks/home.csv "https://docs.google.com/spreadsheets/d/15LA25I-z8uz6UaF6tXZ9MEMkgbFvanUYTJqlbKdPnhI/export?gid=1198213948&format=csv"
+wget -O tasks/home.csv "https://docs.google.com/spreadsheets/d/15LA25I-z8uz6UaF6tXZ9MEMkgbFvanUYTJqlbKdPnhI/export?gid=1198213948&format=csv"
 
 # Download interventions.csv
-wget --header -O tasks/interventions.csv "https://docs.google.com/spreadsheets/d/15LA25I-z8uz6UaF6tXZ9MEMkgbFvanUYTJqlbKdPnhI/export?gid=1925262691&format=csv"
+wget -O tasks/interventions.csv "https://docs.google.com/spreadsheets/d/15LA25I-z8uz6UaF6tXZ9MEMkgbFvanUYTJqlbKdPnhI/export?gid=1925262691&format=csv"
 
 # Download intents.csv
-wget --header -O tasks/intents.csv "https://docs.google.com/spreadsheets/d/15LA25I-z8uz6UaF6tXZ9MEMkgbFvanUYTJqlbKdPnhI/export?gid=301715508&format=csv"
+wget -O tasks/intents.csv "https://docs.google.com/spreadsheets/d/15LA25I-z8uz6UaF6tXZ9MEMkgbFvanUYTJqlbKdPnhI/export?gid=301715508&format=csv"
